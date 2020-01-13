302c9e2219d62f9b0b8c874670233d903e78b1c4
Ale Battisti <alebat25@yahoo.it>
2020-01-13 19:25:30 +0100

    Packed c3d extractor

--- extract_c3d_sports1M.py
+++ extract_c3d_sports1M.py
@@ -22,15 +22,12 @@ from __future__ import print_function
 import cv2
 import numpy as np
 import os
-import tensorflow.compat.v1 as tf
+import tensorflow as tf
 
 # Basic model parameters as external flags.
 from C3D_tensorflow import c3d_model
-from src.exp.bboxes import read_images
 
-flags = tf.app.flags
-flags.DEFINE_integer('batch_size', 10, 'Batch size.')
-FLAGS = flags.FLAGS
+BATCH_SIZE = 10
 
 
 def placeholder_inputs(batch_size):
@@ -70,43 +67,48 @@ def _variable_with_weight_decay(name, shape, stddev, wd):
     return var
 
 
-def read_batches_of_clips(paths, batch_size, num_frames_per_clip=16, stride=8, resize_size=112):
-    clips = []
-    names = []
-    for clip, name in read_clips(paths, num_frames_per_clip, stride, resize_size):
-        if len(clips) >= batch_size:
-            yield np.array(clips), names
-            clips = []
-            names = []
-        clips.append(clip)
-        names.append(name)
+def read_batches_of_clips(videos, batch_size, num_frames_per_clip=16, stride=8, resize_size=112):
+    clips = read_clips(videos, num_frames_per_clip, stride, resize_size)
+    batch = []
+    descriptors = []
+    for clip, path, frame in clips:
+        if len(batch) >= batch_size:
+            yield np.array(batch), descriptors
+            batch = []
+            descriptors = []
+        batch.append(clip)
+        descriptors.append((path, frame))
 
 
-def read_clips(paths, num_frames_per_clip, stride, resize_size):
+def read_clips(videos, num_frames_per_clip, stride, resize_size):
     assert stride < num_frames_per_clip, "stride < num_frames_per_clip"
-    for path in paths:
-        vc = cv2.VideoCapture(path)
-        clip = []
-        first_frame_n = 0
-        for _, frame in read_images(vc):
-            if len(clip) >= num_frames_per_clip:
-                yield np.array(clip).astype(np.float32), (path, first_frame_n)
-                clip = clip[:num_frames_per_clip - stride]
-                first_frame_n += stride
-            img = np.array(
-                cv2.resize(frame, (resize_size, resize_size), interpolation=cv2.INTER_LINEAR)
-            ).astype(np.float32)
-            clip.append(img)
-        # if piece of clip remains then pad it
-        if len(clip) > 0:
-            for frame in range(len(clip), num_frames_per_clip):
-                clip.append(clip[-1])
-            yield np.array(clip).astype(np.float32), (path, first_frame_n)
+    for images, path in videos:
+        yield from make_clips(images, num_frames_per_clip, path, resize_size, stride)
+
+
+# Padding is 'hold'
+def make_clips(images, num_frames_per_clip, path, resize_size, stride):
+    clip = []
+    first_frame_n = 0
+    for _, frame in images:
+        if len(clip) >= num_frames_per_clip:
+            yield np.array(clip).astype(np.float32), path, first_frame_n
+            clip = clip[:num_frames_per_clip - stride]
+            first_frame_n += stride
+        img = np.array(
+            cv2.resize(frame, (resize_size, resize_size), interpolation=cv2.INTER_LINEAR)
+        ).astype(np.float32)
+        clip.append(img)
+    # if piece of clip remains then pad it
+    if len(clip) > 0:
+        for frame in range(len(clip), num_frames_per_clip):
+            clip.append(clip[-1])
+        yield np.array(clip).astype(np.float32), path, first_frame_n
 
 
 def run_test():
     model_name = "./data/conv3d_deepnetA_sport1m_iter_1900000_TF.model"
-    path = 'runs/test_crop/6'
+    path = '6'
     test_videos = list(
         map(
             lambda x: os.path.join(path, x),
@@ -117,7 +119,7 @@ def run_test():
     )
     print("Number of test videos={}".format(len(test_videos)))
 
-    batch_size = FLAGS.batch_size
+    batch_size = BATCH_SIZE
 
     # Get the sets of images and labels for training, validation, and
     images_placeholder, labels_placeholder = placeholder_inputs(batch_size)
@@ -149,10 +151,10 @@ def run_test():
             'out': _variable_with_weight_decay('bout', [c3d_model.NUM_CLASSES], 0.04, 0.0),
         }
     logits = []
-    with tf.device('/gpu:0'):
+    with tf.device('/gpu:1'):
         logit = c3d_model.inference_c3d(
-            images_placeholder[0:FLAGS.batch_size, :, :, :, :], 0.6,
-            FLAGS.batch_size, weights, biases)
+            images_placeholder[0:BATCH_SIZE, :, :, :, :], 0.6,
+            BATCH_SIZE, weights, biases)
         logits.append(logit)
     logits = tf.concat(logits, 0)
     saver = tf.train.Saver()
@@ -178,3 +180,60 @@ def main(_):
 
 if __name__ == '__main__':
     tf.app.run()
+
+
+def get_weights_and_biases():
+    with tf.variable_scope('var_name'):
+        weights = {
+            'wc1': _variable_with_weight_decay('wc1', [3, 3, 3, 3, 64], 0.04, 0.00),
+            'wc2': _variable_with_weight_decay('wc2', [3, 3, 3, 64, 128], 0.04, 0.00),
+            'wc3a': _variable_with_weight_decay('wc3a', [3, 3, 3, 128, 256], 0.04, 0.00),
+            'wc3b': _variable_with_weight_decay('wc3b', [3, 3, 3, 256, 256], 0.04, 0.00),
+            'wc4a': _variable_with_weight_decay('wc4a', [3, 3, 3, 256, 512], 0.04, 0.00),
+            'wc4b': _variable_with_weight_decay('wc4b', [3, 3, 3, 512, 512], 0.04, 0.00),
+            'wc5a': _variable_with_weight_decay('wc5a', [3, 3, 3, 512, 512], 0.04, 0.00),
+            'wc5b': _variable_with_weight_decay('wc5b', [3, 3, 3, 512, 512], 0.04, 0.00),
+            'wd1': _variable_with_weight_decay('wd1', [8192, 4096], 0.04, 0.001),
+            'wd2': _variable_with_weight_decay('wd2', [4096, 4096], 0.04, 0.002),
+            'out': _variable_with_weight_decay('wout', [4096, c3d_model.NUM_CLASSES], 0.04, 0.005)
+        }
+        biases = {
+            'bc1': _variable_with_weight_decay('bc1', [64], 0.04, 0.0),
+            'bc2': _variable_with_weight_decay('bc2', [128], 0.04, 0.0),
+            'bc3a': _variable_with_weight_decay('bc3a', [256], 0.04, 0.0),
+            'bc3b': _variable_with_weight_decay('bc3b', [256], 0.04, 0.0),
+            'bc4a': _variable_with_weight_decay('bc4a', [512], 0.04, 0.0),
+            'bc4b': _variable_with_weight_decay('bc4b', [512], 0.04, 0.0),
+            'bc5a': _variable_with_weight_decay('bc5a', [512], 0.04, 0.0),
+            'bc5b': _variable_with_weight_decay('bc5b', [512], 0.04, 0.0),
+            'bd1': _variable_with_weight_decay('bd1', [4096], 0.04, 0.0),
+            'bd2': _variable_with_weight_decay('bd2', [4096], 0.04, 0.0),
+            'out': _variable_with_weight_decay('bout', [c3d_model.NUM_CLASSES], 0.04, 0.0),
+        }
+    return biases, weights
+
+
+def extract_c3d(batch_size, device, model_name, named_videos):
+    images_placeholder = tf.placeholder(
+        tf.float32,
+        shape=(batch_size,
+               c3d_model.NUM_FRAMES_PER_CLIP,
+               c3d_model.CROP_SIZE,
+               c3d_model.CROP_SIZE,
+               c3d_model.CHANNELS))
+    biases, weights = get_weights_and_biases()
+    with tf.device(device):
+        logit = c3d_model.inference_c3d(
+            images_placeholder[0:BATCH_SIZE, :, :, :, :], 0.6,
+            BATCH_SIZE, weights, biases)
+    saver = tf.train.Saver()
+    sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True))
+    init = tf.global_variables_initializer()
+    sess.run(init)
+    saver.restore(sess, model_name)
+    for clips, descriptors in read_batches_of_clips(named_videos, batch_size):
+        predictions = logit.eval(
+            session=sess,
+            feed_dict={images_placeholder: clips}
+        )
+        yield from zip(predictions, descriptors)
