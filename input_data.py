4c9df12ef216d656738e57583730a43ae82fe257
Ale Battisti <alebat25@yahoo.it>
2020-01-13 11:24:51 +0100

    Trim and adaptation to scenario

--- input_data.py
+++ input_data.py
@@ -18,89 +18,37 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import os
-from six.moves import xrange  # pylint: disable=redefined-builtin
-import tensorflow as tf
-import PIL.Image as Image
-import random
-import numpy as np
 import cv2
-import time
+import numpy as np
 
-def get_frames_data(filename, num_frames_per_clip=16):
-  ''' Given a directory containing extracted frames, return a video clip of
-  (num_frames_per_clip) consecutive frames as a list of np arrays '''
-  ret_arr = []
-  s_index = 0
-  for parent, dirnames, filenames in os.walk(filename):
-    if(len(filenames)<num_frames_per_clip):
-      return [], s_index
-    filenames = sorted(filenames)
-    s_index = random.randint(0, len(filenames) - num_frames_per_clip)
-    for i in range(s_index, s_index + num_frames_per_clip):
-      image_name = str(filename) + '/' + str(filenames[i])
-      img = Image.open(image_name)
-      img_data = np.array(img)
-      ret_arr.append(img_data)
-  return ret_arr, s_index
+from src.bboxes import read_images
 
-def read_clip_and_label(filename, batch_size, start_pos=-1, num_frames_per_clip=16, crop_size=112, shuffle=False):
-  lines = open(filename,'r')
-  read_dirnames = []
-  data = []
-  label = []
-  batch_index = 0
-  next_batch_start = -1
-  lines = list(lines)
-  np_mean = np.load('crop_mean.npy').reshape([num_frames_per_clip, crop_size, crop_size, 3])
-  # Forcing shuffle, if start_pos is not specified
-  if start_pos < 0:
-    shuffle = True
-  if shuffle:
-    video_indices = range(len(lines))
-    random.seed(time.time())
-    random.shuffle(video_indices)
-  else:
-    # Process videos sequentially
-    video_indices = range(start_pos, len(lines))
-  for index in video_indices:
-    if(batch_index>=batch_size):
-      next_batch_start = index
-      break
-    line = lines[index].strip('\n').split()
-    dirname = line[0]
-    tmp_label = line[1]
-    if not shuffle:
-      print("Loading a video clip from {}...".format(dirname))
-    tmp_data, _ = get_frames_data(dirname, num_frames_per_clip)
-    img_datas = [];
-    if(len(tmp_data)!=0):
-      for j in xrange(len(tmp_data)):
-        img = Image.fromarray(tmp_data[j].astype(np.uint8))
-        if(img.width>img.height):
-          scale = float(crop_size)/float(img.height)
-          img = np.array(cv2.resize(np.array(img),(int(img.width * scale + 1), crop_size))).astype(np.float32)
-        else:
-          scale = float(crop_size)/float(img.width)
-          img = np.array(cv2.resize(np.array(img),(crop_size, int(img.height * scale + 1)))).astype(np.float32)
-        crop_x = int((img.shape[0] - crop_size)/2)
-        crop_y = int((img.shape[1] - crop_size)/2)
-        img = img[crop_x:crop_x+crop_size, crop_y:crop_y+crop_size,:] - np_mean[j]
-        img_datas.append(img)
-      data.append(img_datas)
-      label.append(int(tmp_label))
-      batch_index = batch_index + 1
-      read_dirnames.append(dirname)
 
-  # pad (duplicate) data/label if less than batch_size
-  valid_len = len(data)
-  pad_len = batch_size - valid_len
-  if pad_len:
-    for i in range(pad_len):
-      data.append(img_datas)
-      label.append(int(tmp_label))
+def read_batches_of_clips(paths, batch_size, num_frames_per_clip=16, stride=8, resize_size=112):
+    clips = []
+    names = []
+    for clip, name in read_clips(paths, num_frames_per_clip, stride, resize_size):
+        if len(clip) >= batch_size:
+            yield np.array(clips), names
+        clips.append(clip)
+        names.append(name)
 
-  np_arr_data = np.array(data).astype(np.float32)
-  np_arr_label = np.array(label).astype(np.int64)
 
-  return np_arr_data, np_arr_label, next_batch_start, read_dirnames, valid_len
+def read_clips(paths, num_frames_per_clip, stride, resize_size):
+    assert stride < num_frames_per_clip, "stride < num_frames_per_clip"
+    for path in paths:
+        vc = cv2.VideoCapture(path)
+        clip = []
+        first_frame_n = 0
+        for frame in read_images(vc):
+            if len(clip) >= num_frames_per_clip:
+                yield np.array(clip).astype(np.float32), (path, first_frame_n)
+                clip = clip[:num_frames_per_clip - stride]
+                first_frame_n += stride
+            img = np.array(cv2.resize(np.array(frame), (resize_size, resize_size))).astype(np.float32)
+            clip.append(img)
+        # if piece of clip remains then pad it
+        if len(clip) > 0:
+            for frame in range(len(clip), num_frames_per_clip):
+                clip[frame] = clip[-1]
+            yield np.array(clip).astype(np.float32), (path, first_frame_n)
--- predict_c3d_ucf101.py
+++ predict_c3d_ucf101.py
@@ -19,22 +19,24 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import os.path
-import time
-from six.moves import xrange  # pylint: disable=redefined-builtin
-import tensorflow as tf
-import input_data
-import c3d_model
+import os
+
+import cv2
 import numpy as np
+import tensorflow as tf
 
 # Basic model parameters as external flags.
+from C3D_tensorflow import c3d_model
+from src.bboxes import read_images
+
 flags = tf.app.flags
 gpu_num = 2
-flags.DEFINE_integer('batch_size', 10 , 'Batch size.')
+flags.DEFINE_integer('batch_size', 10, 'Batch size.')
 FLAGS = flags.FLAGS
 
+
 def placeholder_inputs(batch_size):
-  """Generate placeholder variables to represent the input tensors.
+    """Generate placeholder variables to represent the input tensors.
   These placeholders are used as inputs by the rest of the model building
   code and will be fed from the downloaded data in the .run() loop, below.
   Args:
@@ -43,40 +45,84 @@ def placeholder_inputs(batch_size):
     images_placeholder: Images placeholder.
     labels_placeholder: Labels placeholder.
   """
-  # Note that the shapes of the placeholders match the shapes of the full
-  # image and label tensors, except the first dimension is now batch_size
-  # rather than the full size of the train or test data sets.
-  images_placeholder = tf.placeholder(tf.float32, shape=(batch_size,
-                                                         c3d_model.NUM_FRAMES_PER_CLIP,
-                                                         c3d_model.CROP_SIZE,
-                                                         c3d_model.CROP_SIZE,
-                                                         c3d_model.CHANNELS))
-  labels_placeholder = tf.placeholder(tf.int64, shape=(batch_size))
-  return images_placeholder, labels_placeholder
+    # Note that the shapes of the placeholders match the shapes of the full
+    # image and label tensors, except the first dimension is now batch_size
+    # rather than the full size of the train or test data sets.
+    images_placeholder = tf.placeholder(tf.float32, shape=(batch_size,
+                                                           c3d_model.NUM_FRAMES_PER_CLIP,
+                                                           c3d_model.CROP_SIZE,
+                                                           c3d_model.CROP_SIZE,
+                                                           c3d_model.CHANNELS))
+    labels_placeholder = tf.placeholder(tf.int64, shape=batch_size)
+    return images_placeholder, labels_placeholder
+
 
 def _variable_on_cpu(name, shape, initializer):
-  #with tf.device('/cpu:%d' % cpu_id):
-  with tf.device('/cpu:0'):
-    var = tf.get_variable(name, shape, initializer=initializer)
-  return var
+    # with tf.device('/cpu:%d' % cpu_id):
+    with tf.device('/cpu:0'):
+        var = tf.get_variable(name, shape, initializer=initializer)
+    return var
+
 
 def _variable_with_weight_decay(name, shape, stddev, wd):
-  var = _variable_on_cpu(name, shape, tf.truncated_normal_initializer(stddev=stddev))
-  if wd is not None:
-    weight_decay = tf.nn.l2_loss(var) * wd
-    tf.add_to_collection('losses', weight_decay)
-  return var
+    var = _variable_on_cpu(name, shape, tf.truncated_normal_initializer(stddev=stddev))
+    if wd is not None:
+        weight_decay = tf.nn.l2_loss(var) * wd
+        tf.add_to_collection('losses', weight_decay)
+    return var
+
+
+def read_batches_of_clips(paths, batch_size, num_frames_per_clip=16, stride=8, resize_size=112):
+    clips = []
+    names = []
+    for clip, name in read_clips(paths, num_frames_per_clip, stride, resize_size):
+        if len(clips) >= batch_size:
+            yield np.array(clips), names
+        clips.append(clip)
+        names.append(name)
+
+
+def read_clips(paths, num_frames_per_clip, stride, resize_size):
+    assert stride < num_frames_per_clip, "stride < num_frames_per_clip"
+    for path in paths:
+        vc = cv2.VideoCapture(path)
+        clip = []
+        first_frame_n = 0
+        for _, frame in read_images(vc):
+            if len(clip) >= num_frames_per_clip:
+                yield np.array(clip).astype(np.float32), (path, first_frame_n)
+                clip = clip[:num_frames_per_clip - stride]
+                first_frame_n += stride
+            img = np.array(
+                cv2.resize(frame, (resize_size, resize_size), interpolation=cv2.INTER_LINEAR)
+            ).astype(np.float32)
+            clip.append(img)
+        # if piece of clip remains then pad it
+        if len(clip) > 0:
+            for frame in range(len(clip), num_frames_per_clip):
+                clip.append(clip[-1])
+            yield np.array(clip).astype(np.float32), (path, first_frame_n)
+
 
 def run_test():
-  model_name = "./sports1m_finetuning_ucf101.model"
-  test_list_file = 'list/test.list'
-  num_test_videos = len(list(open(test_list_file,'r')))
-  print("Number of test videos={}".format(num_test_videos))
+    model_name = "./data/conv3d_deepnetA_sport1m_iter_1900000_TF.model"
+    path = 'runs/test_crop/6'
+    test_videos = list(
+        map(
+            lambda x: os.path.join(path, x),
+            filter(lambda x: x[-4:] == '.mov',
+                   os.listdir(path)
+                   )
+        )
+    )
+    print("Number of test videos={}".format(len(test_videos)))
 
-  # Get the sets of images and labels for training, validation, and
-  images_placeholder, labels_placeholder = placeholder_inputs(FLAGS.batch_size * gpu_num)
-  with tf.variable_scope('var_name') as var_scope:
-    weights = {
+    batch_size = FLAGS.batch_size * gpu_num
+
+    # Get the sets of images and labels for training, validation, and
+    images_placeholder, labels_placeholder = placeholder_inputs(batch_size)
+    with tf.variable_scope('var_name') as var_scope:
+        weights = {
             'wc1': _variable_with_weight_decay('wc1', [3, 3, 3, 3, 64], 0.04, 0.00),
             'wc2': _variable_with_weight_decay('wc2', [3, 3, 3, 64, 128], 0.04, 0.00),
             'wc3a': _variable_with_weight_decay('wc3a', [3, 3, 3, 128, 256], 0.04, 0.00),
@@ -88,8 +134,8 @@ def run_test():
             'wd1': _variable_with_weight_decay('wd1', [8192, 4096], 0.04, 0.001),
             'wd2': _variable_with_weight_decay('wd2', [4096, 4096], 0.04, 0.002),
             'out': _variable_with_weight_decay('wout', [4096, c3d_model.NUM_CLASSES], 0.04, 0.005)
-            }
-    biases = {
+        }
+        biases = {
             'bc1': _variable_with_weight_decay('bc1', [64], 0.04, 0.0),
             'bc2': _variable_with_weight_decay('bc2', [128], 0.04, 0.0),
             'bc3a': _variable_with_weight_decay('bc3a', [256], 0.04, 0.0),
@@ -101,53 +147,35 @@ def run_test():
             'bd1': _variable_with_weight_decay('bd1', [4096], 0.04, 0.0),
             'bd2': _variable_with_weight_decay('bd2', [4096], 0.04, 0.0),
             'out': _variable_with_weight_decay('bout', [c3d_model.NUM_CLASSES], 0.04, 0.0),
-            }
-  logits = []
-  for gpu_index in range(0, gpu_num):
-    with tf.device('/gpu:%d' % gpu_index):
-      logit = c3d_model.inference_c3d(images_placeholder[gpu_index * FLAGS.batch_size:(gpu_index + 1) * FLAGS.batch_size,:,:,:,:], 0.6, FLAGS.batch_size, weights, biases)
-      logits.append(logit)
-  logits = tf.concat(logits,0)
-  norm_score = tf.nn.softmax(logits)
-  saver = tf.train.Saver()
-  sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True))
-  init = tf.global_variables_initializer()
-  sess.run(init)
-  # Create a saver for writing training checkpoints.
-  saver.restore(sess, model_name)
-  # And then after everything is built, start the training loop.
-  bufsize = 0
-  write_file = open("predict_ret.txt", "w+", bufsize)
-  next_start_pos = 0
-  all_steps = int((num_test_videos - 1) / (FLAGS.batch_size * gpu_num) + 1)
-  for step in xrange(all_steps):
-    # Fill a feed dictionary with the actual set of images and labels
-    # for this particular training step.
-    start_time = time.time()
-    test_images, test_labels, next_start_pos, _, valid_len = \
-            input_data.read_clip_and_label(
-                    test_list_file,
-                    FLAGS.batch_size * gpu_num,
-                    start_pos=next_start_pos
-                    )
-    predict_score = norm_score.eval(
+        }
+    logits = []
+    for gpu_index in range(0, gpu_num):
+        with tf.device('/gpu:%d' % gpu_index):
+            logit = c3d_model.inference_c3d(
+                images_placeholder[gpu_index * FLAGS.batch_size:(gpu_index + 1) * FLAGS.batch_size, :, :, :, :], 0.6,
+                FLAGS.batch_size, weights, biases)
+            logits.append(logit)
+    logits = tf.concat(logits, 0)
+    saver = tf.train.Saver()
+    sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True))
+    init = tf.global_variables_initializer()
+    sess.run(init)
+    # Create a saver for writing training checkpoints.
+    saver.restore(sess, model_name)
+    # And then after everything is built, start the training loop.
+    for clips, names in read_batches_of_clips(test_videos, batch_size):
+        prediction = logits.eval(
             session=sess,
-            feed_dict={images_placeholder: test_images}
-            )
-    for i in range(0, valid_len):
-      true_label = test_labels[i],
-      top1_predicted_label = np.argmax(predict_score[i])
-      # Write results: true label, class prob for true label, predicted label, class prob for predicted label
-      write_file.write('{}, {}, {}, {}\n'.format(
-              true_label[0],
-              predict_score[i][true_label],
-              top1_predicted_label,
-              predict_score[i][top1_predicted_label]))
-  write_file.close()
-  print("done")
+            feed_dict={images_placeholder: clips}
+        )
+        print(prediction.shape)
+        print(prediction)
+    print("done")
+
 
 def main(_):
-  run_test()
+    run_test()
+
 
 if __name__ == '__main__':
-  tf.app.run()
+    tf.app.run()
